28f36b3d4f5b77559d95b13230320cfc1e34fde1
Std <student@Lab-1302.(none)>
2013-01-26 15:18:35 +0700

    complete homework

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @post = Post.new( body: "Sawaddee^____^", title: "Write here!!!") }
+  subject {@post}
+
+  it { should respond_to(:body) }
+  it { should respond_to(:title) }
+
+ describe "when Body format is valid" do
+ 	before { @post.body = "" }
+ 	it { should_not be_valid }
+  end
+
+ describe "when title format is valid" do
+ 	before { @post.title = "" }
+ 	it { should_not be_valid }
+  end  
 end
