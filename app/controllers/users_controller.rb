44a6aeb8d20c74135e5f99ee6015dae5ad5628d3
Std <student@Lab-1302.(none)>
2013-02-05 16:16:39 +0700

    Create index update edit destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
 
-	def index
-	end
 
+def index
+  @user = User.all
+ 
+  respond_to do |format|
+    format.html  # index.html.erb
+    format.json  { render :json => @user }
+  end
+end
 	def new
 		@user = User.new
 	end
@@ -21,12 +27,33 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		 @user = User.find(params[:id])
+ 
+  respond_to do |format|
+    if @user.update_attributes(params[:post])
+      format.html  { redirect_to(@user,
+                    :notice => 'Post was successfully updated.') }
+      format.json  { head :no_content }
+    else
+      format.html  { render :action => "edit" }
+      format.json  { render :json => @user.errors,
+                    :status => :unprocessable_entity }
+    end
+  end
 	end
 
 	def destroy
+		@user = User.find(params[:id])
+  @user.destroy
+ 
+  respond_to do |format|
+    format.html { redirect_to users_url }
+    format.json { head :no_content }
+  end
 	end
 
 end	
